f6d195ef192af3bcba10fb32f4dd938fa968a430
neosan <nilva3465@gmail.com>
2024-12-23 21:42:56 +0900

    domain service testでけた

--- crates/domain-service/src/repository.rs
+++ crates/domain-service/src/repository.rs
@@ -4,12 +4,8 @@ use domain_model::{err::ServiceError, model::UserName};
 use mockall::automock;
 
 #[cfg_attr(test, automock)]
-pub trait CheckUserNameNgWordRepository {
+pub trait RegisterUserNameRepository {
     fn find_ng_word(&self, user_name: &UserName) -> Result<bool, ServiceError>;
     // SELECT count(*) FROM ng_word WHERE ? LIKE CONCAT('%', word, '%'); みたいな感じで実装する
-}
-
-#[cfg_attr(test, automock)]
-pub trait CreateUserNameRepository {
     fn create_user_name(&self, user_name: &UserName) -> Result<(), ServiceError>;
 }
--- crates/domain-service/src/service.rs
+++ crates/domain-service/src/service.rs
@@ -1,4 +1,4 @@
-use crate::repository::{CheckUserNameNgWordRepository, CreateUserNameRepository};
+use crate::repository::RegisterUserNameRepository;
 use domain_model::{
     err::{RegisterUserNameError, ValidationError},
     model::{UnvalidatedUserName, UserName},
@@ -6,17 +6,12 @@ use domain_model::{
 use std::sync::Arc;
 
 pub struct RegisterUserNameService {
-    check_user_name_ng_word_repository: Arc<dyn CheckUserNameNgWordRepository>,
-    create_user_name_repository: Arc<dyn CreateUserNameRepository>,
+    register_user_name_repository: Arc<dyn RegisterUserNameRepository>,
 }
 impl RegisterUserNameService {
-    pub fn new(
-        check_user_name_ng_word_repository: Arc<dyn CheckUserNameNgWordRepository>,
-        create_user_name_repository: Arc<dyn CreateUserNameRepository>,
-    ) -> Self {
+    pub fn new(register_user_name_repository: Arc<dyn RegisterUserNameRepository>) -> Self {
         Self {
-            check_user_name_ng_word_repository,
-            create_user_name_repository,
+            register_user_name_repository,
         }
     }
 
@@ -27,7 +22,7 @@ impl RegisterUserNameService {
         let user_name = UserName::new(&user_name.0)?;
 
         if self
-            .check_user_name_ng_word_repository
+            .register_user_name_repository
             .find_ng_word(&user_name)?
         {
             return Err(RegisterUserNameError::from(ValidationError(
@@ -35,14 +30,12 @@ impl RegisterUserNameService {
             )));
         }
 
-        if self
-            .create_user_name_repository
-            .create_user_name(&user_name)
-            .is_err()
-        {
-            return Err(RegisterUserNameError::from(ValidationError(
-                "Failed to register user name".to_string(),
-            )));
+        let register_result = self
+            .register_user_name_repository
+            .create_user_name(&user_name);
+
+        if let Err(err) = register_result {
+            return Err(RegisterUserNameError::from(err));
         }
 
         Ok(())
@@ -55,40 +48,28 @@ mod tests {
     use mockall::predicate::*;
 
     mod register_user_name_service {
-        use crate::repository::{MockCheckUserNameNgWordRepository, MockCreateUserNameRepository};
+        use domain_model::err::ServiceError;
 
         use super::*;
-
-        fn service_build_with_mock(
-            check_user_name_ng_word_repository: Arc<dyn CheckUserNameNgWordRepository>,
-            create_user_name_repository: Arc<dyn CreateUserNameRepository>,
-        ) -> RegisterUserNameService {
-            RegisterUserNameService::new(
-                check_user_name_ng_word_repository,
-                create_user_name_repository,
-            )
-        }
+        use crate::repository::MockRegisterUserNameRepository;
 
         #[test]
         fn test_register_user_name() {
-            let mut check_user_name_ng_word_repository = MockCheckUserNameNgWordRepository::new();
-            check_user_name_ng_word_repository
+            let mut register_user_name_repository = MockRegisterUserNameRepository::new();
+
+            register_user_name_repository
                 .expect_find_ng_word()
                 .times(1)
                 .with(eq(UserName::new("word").unwrap()))
                 .returning(|_| Ok(false));
 
-            let mut create_user_name_repository = MockCreateUserNameRepository::new();
-            create_user_name_repository
+            register_user_name_repository
                 .expect_create_user_name()
                 .times(1)
                 .with(eq(UserName::new("word").unwrap()))
                 .returning(|_| Ok(()));
 
-            let service = service_build_with_mock(
-                Arc::new(check_user_name_ng_word_repository),
-                Arc::new(create_user_name_repository),
-            );
+            let service = RegisterUserNameService::new(Arc::new(register_user_name_repository));
 
             let result = service.register_user_name(UnvalidatedUserName("word".to_string()));
 
@@ -96,45 +77,130 @@ mod tests {
         }
 
         #[test]
-        #[ignore]
-        fn test_register_user_name_ng_word() {
-            // モックを
-            // NGワードが含まれる場合
-        }
-
-        #[test]
-        #[ignore]
-        fn test_register_user_name_already_exists() {
-            // モックを
-            // 既に登録されている場合
-        }
-
-        #[test]
-        #[ignore]
         fn test_register_user_name_too_long() {
-            // モックを
-            // 20文字を超える場合
+            let mut register_user_name_repository = MockRegisterUserNameRepository::new();
+
+            register_user_name_repository.expect_find_ng_word().times(0);
+
+            register_user_name_repository
+                .expect_create_user_name()
+                .times(0);
+
+            let service = RegisterUserNameService::new(Arc::new(register_user_name_repository));
+
+            let result =
+                service.register_user_name(UnvalidatedUserName("veryverylongusern".to_string()));
+
+            assert_eq!(
+                result,
+                Err(RegisterUserNameError::ValidationError(ValidationError(
+                    "Username must be at most 16 characters long".to_string()
+                )))
+            );
         }
 
         #[test]
-        #[ignore]
         fn test_register_user_name_too_short() {
-            // モックを
-            // 3文字未満の場合
+            let mut register_user_name_repository = MockRegisterUserNameRepository::new();
+
+            register_user_name_repository.expect_find_ng_word().times(0);
+
+            register_user_name_repository
+                .expect_create_user_name()
+                .times(0);
+
+            let service = RegisterUserNameService::new(Arc::new(register_user_name_repository));
+
+            let result = service.register_user_name(UnvalidatedUserName("sh".to_string()));
+
+            assert_eq!(
+                result,
+                Err(RegisterUserNameError::ValidationError(ValidationError(
+                    "Username must be at least 3 characters long".to_string()
+                )))
+            );
         }
 
         #[test]
-        #[ignore]
-        fn test_register_user_name_too_check_ng_word_repository_error() {
-            // モックを
-            // NGワードリポジトリでのエラー
+        fn test_register_user_name_ng_word() {
+            let mut register_user_name_repository = MockRegisterUserNameRepository::new();
+
+            register_user_name_repository
+                .expect_find_ng_word()
+                .times(1)
+                .with(eq(UserName::new("word").unwrap()))
+                .returning(|_| Ok(true));
+
+            register_user_name_repository
+                .expect_create_user_name()
+                .times(0)
+                .with(eq(UserName::new("word").unwrap()))
+                .returning(|_| Ok(()));
+
+            let service = RegisterUserNameService::new(Arc::new(register_user_name_repository));
+
+            let result = service.register_user_name(UnvalidatedUserName("word".to_string()));
+
+            assert_eq!(
+                result,
+                Err(RegisterUserNameError::ValidationError(ValidationError(
+                    "Name must not contain NG words".to_string()
+                )))
+            );
         }
 
         #[test]
-        #[ignore]
-        fn test_register_user_name_too_register_user_name_repository_error() {
-            // モックを
-            // ユーザーネームリポジトリでのエラー
+        fn test_register_user_name_too_register_user_name_error() {
+            let mut register_user_name_repository = MockRegisterUserNameRepository::new();
+
+            register_user_name_repository
+                .expect_find_ng_word()
+                .times(1)
+                .with(eq(UserName::new("word").unwrap()))
+                .returning(|_| Ok(false));
+
+            register_user_name_repository
+                .expect_create_user_name()
+                .times(1)
+                .with(eq(UserName::new("word").unwrap()))
+                .returning(|_| Err(ServiceError("Already exists".to_string())));
+
+            let service = RegisterUserNameService::new(Arc::new(register_user_name_repository));
+
+            let result = service.register_user_name(UnvalidatedUserName("word".to_string()));
+
+            assert_eq!(
+                result,
+                Err(RegisterUserNameError::ServiceError(ServiceError(
+                    "Already exists".to_string()
+                )))
+            );
+        }
+
+        #[test]
+        fn test_register_user_name_check_ng_word_error() {
+            let mut register_user_name_repository = MockRegisterUserNameRepository::new();
+
+            register_user_name_repository
+                .expect_find_ng_word()
+                .times(1)
+                .with(eq(UserName::new("word").unwrap()))
+                .returning(|_| Err(ServiceError("Service error".to_string())));
+
+            register_user_name_repository
+                .expect_create_user_name()
+                .times(0);
+
+            let service = RegisterUserNameService::new(Arc::new(register_user_name_repository));
+
+            let result = service.register_user_name(UnvalidatedUserName("word".to_string()));
+
+            assert_eq!(
+                result,
+                Err(RegisterUserNameError::from(ServiceError(
+                    "Service error".to_string(),
+                )))
+            );
         }
     }
 }
